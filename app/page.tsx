d8fdde935f33130e94b373bc9ae3a87e516199de
SKRTEEEEEE <adanreh.m@gmail.com>
2024-08-14 12:29:19 +0200

    build(app): Configure shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
@@ -73,7 +74,6 @@ export default function Home() {
             Learn about Next.js in an interactive course with&nbsp;quizzes!
           </p>
         </a>
-
         <a
           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
@@ -90,6 +90,7 @@ export default function Home() {
             Explore starter templates for Next.js.
           </p>
         </a>
+      
 
         <a
           href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
@@ -107,6 +108,9 @@ export default function Home() {
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
         </a>
+        <Button>
+        Shadcn button
+        </Button>
       </div>
     </main>
   );
