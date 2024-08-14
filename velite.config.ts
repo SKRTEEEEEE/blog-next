aa76a9d17e534c4f5fba90fafed182877f7da051
SKRTEEEEEE <adanreh.m@gmail.com>
2024-08-14 18:33:29 +0200

    feat(blog): Create blog page

--- velite.config.ts
+++ velite.config.ts
@@ -13,7 +13,7 @@ const posts = defineCollection({
         title: s.string().max(99),
         description: s.string().max(999).optional(),
         date: s.isodate(),
-        publish: s.boolean().default(true),
+        published: s.boolean().default(true),
         body: s.mdx()
     })
     .transform(computedFields)
