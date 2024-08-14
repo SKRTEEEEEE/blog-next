784115a161509d160958fa863ad3f461d932249b
SKRTEEEEEE <adanreh.m@gmail.com>
2024-08-14 14:04:16 +0200

    feat(app): Navbar not responsive

---
+++ config/site.ts
@@ -0,0 +1,12 @@
+export const siteConfig = {
+    name: "SkrtBlog",
+    url: "https://example.com",
+    description: "NextJs 14 blog using velite, tailwind and shadcn",
+    links:{
+        twitter: "https://x.com/queen420nft",
+        github: "https://github.com/SKRTEEEEEE",
+        personalSite: "https://profile-skrt.vercel.app/"
+    }
+}
+
+export type SiteConfig = typeof siteConfig;
