a262ea89d4aad5c99575f2e8c7ea71a197992483
SKRTEEEEEE <adanreh.m@gmail.com>
2024-08-15 12:07:34 +0200

    feat(...page): Configure mdx styles
    - Add some libraries for manage usefull mdx cases

--- velite.config.ts
+++ velite.config.ts
@@ -1,4 +1,10 @@
+import rehypeSlug from "rehype-slug";
+import rehypePrettyCode from "rehype-pretty-code"
+import rehypeAutolinkHeadings from "rehype-autolink-headings"
 import {defineConfig, defineCollection, s}from "velite";
+
+
+
 // blog/hello-world
 const computedFields = <T extends {slug: string}>(data: T)=>({
     ...data,
@@ -30,7 +36,10 @@ export default defineConfig({
     },
     collections: {posts},
     mdx: {
-        rehypePlugins: [],
+        rehypePlugins: [rehypeSlug,[rehypePrettyCode, {theme: "github-dark"}],[rehypeAutolinkHeadings, { behavior: "wrap",properties: {
+            className: ["subheading-anchor"],
+            ariaLabel: "Link to section",
+        }}]],
         remarkPlugins: []
     }
 })
